69de02bbe083c37ada90e8118af0619742a336d4
Hank Li <hankli0130@gmail.com>
2018-10-29 10:53:55 +0800

    First commit

---
+++ app/src/main/java/tw/hankli/staggeredgridlayoutmanagerdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package tw.hankli.staggeredgridlayoutmanagerdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/tw/hankli/staggeredgridlayoutmanagerdemo/MyAdapter.kt
@@ -0,0 +1,24 @@
+package tw.hankli.staggeredgridlayoutmanagerdemo
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
